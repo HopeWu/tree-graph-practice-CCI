3e94ca91752c779cef3bca86d2844beb8d1b4783
HopeWu <767088240@qq.com>
2023-01-03 10:20:22 +0000

    4.5 Validate BST add another method to check it. Check ranges.

--- src/ValidateBST.java
+++ src/ValidateBST.java
@@ -1,8 +1,13 @@
+import org.jetbrains.annotations.NotNull;
+
 public class ValidateBST {
     public static void main(String[] args) {
         ValidateBST vbst = new ValidateBST();
 
         BinNode node = vbst.initializeTree();
+
+        // One way
+
         try {
             int max = vbst.maxOf(node);
             System.out.println("This is a BST.");
@@ -11,8 +16,49 @@ public class ValidateBST {
         }catch (NullTreeException e){
             System.out.println(e);
         }
+
+
+        // Another way
+//        if(vbst.checkBSTRange(node, (int) Double.NEGATIVE_INFINITY, (int) Double.POSITIVE_INFINITY))
+//            System.out.println("True BST!");
+//        else
+//            System.out.println("False BST!");
     }
 
+    /*
+    Another method
+     */
+    private boolean checkBSTRange(BinNode tree, int min, int max){
+        if(tree.data > max)
+        {
+            System.out.println("Violated BFS rule. " + tree.data + " is greater than " + max);
+            return false;
+        }
+        if(tree.data < min)
+        {
+            System.out.println("Violated BFS rule. " + tree.data + " is smaller than " + min);
+            return false;
+        }
+
+        int _max;
+        if (tree.data < max) _max = tree.data;
+        else _max = max;
+        boolean flag = checkBSTRange(tree.left, min, _max);
+        if (!flag) {
+            return false;
+        }
+
+        int _min;
+        if (tree.data > min) _min = tree.data;
+        else _min = min;
+        flag &= checkBSTRange(tree.right, _min, max);
+
+        return flag;
+    }
+
+    /*
+    One method
+     */
     private int maxOf(BinNode tree) throws ViolateBfsException, NullTreeException{
         if(tree == null) throw new NullTreeException("The input tree is null.");
 
