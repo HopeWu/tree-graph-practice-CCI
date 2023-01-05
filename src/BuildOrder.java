b6348d863eef1c0d585905425dbb5710eeda64c0
HopeWu <767088240@qq.com>
2023-01-05 11:59:48 +0000

    4.7 Build order complete one method. DFS is used. ImprovedAnotherApproach is used as an improvement.

--- src/BuildOrder.java
+++ src/BuildOrder.java
@@ -1,21 +1,58 @@
-import java.util.ArrayList;
-import java.util.Hashtable;
-import java.util.Set;
-import java.util.Stack;
+import java.util.*;
 
 public class BuildOrder {
 
     static public void main(String[] args){
         BuildOrder buildOrder = new BuildOrder();
         ArrayList<Node> projects = buildOrder.initilize();
-//        ArrayList<Node> order = buildOrder.anotherApproach(projects);
-        AnotherApproach anotherApproach = new AnotherApproach();
+
+//        OneApproach oneApproach = new OneApproach();
+//        ArrayList<Node> order = oneApproach.run(projects);
+//        AnotherApproach anotherApproach = new AnotherApproach();
+        AnotherApproach anotherApproach = new ImprovedAnotherApproach();
         ArrayList<Node> order = anotherApproach.run(projects);
         order.forEach((node -> print(node.data)));
     }
 
+    static class OneApproach{
+        public ArrayList<Node> run(ArrayList<Node> projects){
+            ArrayList<Node> starters = getStarters(projects);
+
+            return null;
+        }
+
+        private ArrayList<Node> oneApproach(ArrayList<Node> projects){
+            Hashtable<String, Integer> hashtable = new Hashtable<>();
+            ArrayList<Node> starters = new ArrayList<>();
+            return null;
+        }
+    }
+    static class ImprovedAnotherApproach extends AnotherApproach{
+        private ArrayList<Node> run(ArrayList<Node> projects){
+            ArrayList<Node> starters = getStarters(projects);
+            for (Node node: starters){
+                dfs(node);
+            }
+            ArrayList<Node> result = new ArrayList<>();
+            while(!stack.isEmpty()) result.add(stack.pop());
+            return result;
+        }
+
+        private void dfs(Node node){
+            ArrayList<Node> adjacents = node.adjacents;
+            if (adjacents.isEmpty()){
+                stack.push(node);
+            }else{
+                for (Node node1: adjacents){
+                    dfs(node1);
+                }
+                stack.push(node);
+            }
+        }
+    }
+
     static class AnotherApproach{
-        private Stack<Node> stack = new Stack<>();
+        protected Stack<Node> stack = new Stack<>();
 
         private ArrayList<Node> run(ArrayList<Node> projects)
         {
@@ -29,6 +66,7 @@ public class BuildOrder {
                     break;
                 }
             }
+
             ArrayList<Node> result = new ArrayList<>();
             while(!stack.isEmpty()) result.add(stack.pop());
             return result;
@@ -55,18 +93,24 @@ public class BuildOrder {
             }
         }
     }
+    static private ArrayList<Node> getStarters(ArrayList<Node> projects){
+        Hashtable<Node, Integer> hashtable = new Hashtable<>();
+        for (Node node: projects){
+            hashtable.put(node, 0);
+        }
+        for (Node node: projects){
+            for( Node n: node.adjacents){
+                hashtable.put(n, 1);
+            }
+        }
+
+        hashtable.values().removeAll(Collections.singleton(1));
+        return new ArrayList<>(hashtable.keySet());
+    }
 
     static public void print(Object obj){
         System.out.println(obj);
     }
-
-    private ArrayList<Node> oneApproach(ArrayList<Node> projects){
-        Hashtable<String, Integer> hashtable = new Hashtable<>();
-        ArrayList<Node> starters = new ArrayList<>();
-        return null;
-    }
-
-
 
     public ArrayList<Node> initilize(){
         ArrayList<Node> projects = new ArrayList<>();
